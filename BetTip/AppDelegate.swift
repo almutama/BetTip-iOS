b86f032bb9f4a04e098f84dc0ff53d41e2dae30c
Haydar Karkin <haydar.karkin@gmail.com>
2017-11-07 23:21:21 +0300

    Added push notification settings

--- BetTip/AppDelegate.swift
+++ BetTip/AppDelegate.swift
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.setupNotification(application: application)
+        self.registerServices()
         return true
     }
 
@@ -34,5 +36,84 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
+    }
+    
+    func setupNotification(application: UIApplication) {
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            
+            // For iOS 10 data message (sent via FCM)
+            Messaging.messaging().delegate = self
+            
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+    }
+    
+    func registerServices() {
+        FirebaseApp.configure()
+        IQKeyboardManager.sharedManager().enable = true
+    }
+}
+
+extension AppDelegate {
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        var token: String = ""
+        for i in 0..<deviceToken.count {
+            token += String(format: "%02.2hhx", deviceToken[i] as CVarArg)
+        }
+        print("APN token is: ", token)
+        Messaging.messaging().setAPNSToken(deviceToken, type: .unknown)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
+        print(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    }
+}
+
+extension AppDelegate : UNUserNotificationCenterDelegate {
+    
+    // Receive displayed notifications for iOS 10 devices.
+    @available(iOS 10.0, *)
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        // Print message ID.
+        print("Message ID: \(String(describing: userInfo["gcm.message_id"]))")
+        
+        // Print full message.
+        print("%@", userInfo)
+    }
+}
+
+extension AppDelegate : MessagingDelegate {
+    
+    public func messaging(_ messaging: Messaging, didRefreshRegistrationToken fcmToken: String){
+        connectToMessaging()
+    }
+    
+    // Receive data message on iOS 10 devices.
+    func application(received remoteMessage: MessagingRemoteMessage) {
+        print("%@", remoteMessage.appData)
+    }
+    
+    func connectToMessaging() {
+        Messaging.messaging().shouldEstablishDirectChannel = true
     }
 }
