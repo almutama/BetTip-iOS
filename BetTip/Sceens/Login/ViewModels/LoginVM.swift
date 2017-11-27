63176db7f70e85fd528b67f0f3cdebef615ebcfe
Haydar Karkin <haydar.karkin@gmail.com>
2017-11-27 17:15:10 +0300

    Added generated code scripts

--- BetTip/Sceens/Login/ViewModels/LoginVM.swift
+++ BetTip/Sceens/Login/ViewModels/LoginVM.swift
@@ -11,9 +11,35 @@ import RxSwift
 
 class LoginVM: BaseViewModel {
     
+    let credentialAuthProvider: CredentialAuthProvider!
     let disposeBag = DisposeBag()
+    var isLoginSuccess = Variable<Bool>(false)
+    
+    init(credentialAuthProvider: CredentialAuthProvider) {
+        self.credentialAuthProvider = credentialAuthProvider
+        super.init()
+    }
     
     func login(email: String, password: String) {
+        let validatedCredentials = loginCredentialsRule.run((email: email, password: password))
         
+        switch validatedCredentials {
+        case .success(let email, let password):
+            print("")
+            let login = self.credentialAuthProvider.login(email: email, password: password)
+                .trackActivity(in: loadingIndicator)
+                .share(replay: 1)
+            
+            login.filterError()
+                .subscribe(onNext: self.loginSuccessful)
+                .disposed(by: disposeBag)
+            
+        case .failure(let error):
+            print(error)
+        }
+    }
+    
+    func loginSuccessful (user: UserModel) {
+        self.isLoginSuccess.value = true
     }
 }
