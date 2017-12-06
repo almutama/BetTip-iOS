a6b1eb58b085f739967475f178988a98737c551f
Haydar Karkin <haydar.karkin@gmail.com>
2017-12-06 17:20:25 +0300

    Added credit cell's new areas

--- BetTip/Sceens/Credit/Views/CreditCell.swift
+++ BetTip/Sceens/Credit/Views/CreditCell.swift
@@ -7,9 +7,28 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class CreditCell: UICollectionViewCell {
+class CreditCell: UICollectionViewCell, Reusable {
 
+    @IBOutlet weak var numberOfCreditLbl: StyledLabel!
+    @IBOutlet weak var priceLbl: StyledLabel!
+    
+    var disposeBag = DisposeBag()
+    var viewModel: Variable<CreditModel> = Variable<CreditModel>.init(CreditModel.init()) {
+        didSet {
+            _ = viewModel.asObservable().observeOn(MainScheduler.instance)
+                .subscribe({ [unowned self] (event) in
+                    if let entity = event.element {
+                        self.numberOfCreditLbl.text = "\(String(describing: entity.numberOfCredits))"
+                        self.priceLbl.text = "\(String(describing: entity.price))"
+                    }
+                })
+                .disposed(by: disposeBag)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
