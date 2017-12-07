365cdd568f32eb578585a84d358d8691faaa1266
Haydar Karkin <haydar.karkin@gmail.com>
2017-12-07 22:59:28 +0300

    Added shake animation

---
+++ BetTip/Shared/Presenters/ShakeViewAnimation.swift
@@ -0,0 +1,22 @@
+//
+//  ShakeView.swift
+//  BetTip
+//
+//  Created by Haydar Karkin on 7.12.2017.
+//  Copyright © 2017 Haydar Karkin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShakeViewAnimation {
+    static func shakeView(view: UIView) {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.06
+        animation.repeatCount = 4
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x:view.center.x - 10, y:view.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x:view.center.x + 10, y:view.center.y))
+        view.layer.add(animation, forKey: "position")
+    }
+}
