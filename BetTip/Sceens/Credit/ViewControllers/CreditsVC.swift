5ebf1d16c30174714122cbf767a6be31b7c8b62e
Haydar Karkin <haydar.karkin@gmail.com>
2018-05-09 22:27:11 +0300

    App iOS version decrease as 10

--- BetTip/Sceens/Credit/ViewControllers/CreditsVC.swift
+++ BetTip/Sceens/Credit/ViewControllers/CreditsVC.swift
@@ -9,7 +9,6 @@
 import UIKit
 import RxSwift
 import VegaScrollFlowLayout
-import PassKit
 
 private let logger = Log.createLogger()
 
@@ -62,7 +61,8 @@ class CreditsVC: BaseViewController {
         self.collectionView.rx.modelSelected(CreditModel.self)
             .subscribe(onNext: { [weak self] credit in
                 if let price = credit.price, let numberOfCredit = credit.numberOfCredits {
-                    self?.makePayment(price: price, numberOfCredit: numberOfCredit)
+                    guard self != nil else { return }
+                    logger.log(.debug, "\(numberOfCredit) consts \(price)")
                 }
             })
             .disposed(by: disposeBag)
@@ -75,57 +75,5 @@ extension CreditsVC: UICollectionViewDelegateFlowLayout {
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.size.width, height: 60)
-    }
-}
-
-extension CreditsVC: PKPaymentAuthorizationViewControllerDelegate {
-    // MARK: - Apple Pay Delegate Methods
-    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
-        logger.log(.debug, "Authorization Finished")
-        controller.dismiss(animated: true, completion: nil)
-    }
-    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController,
-                                            didSelect paymentMethod: PKPaymentMethod,
-                                            handler completion: @escaping (PKPaymentRequestPaymentMethodUpdate) -> Void) {
-        logger.log(.debug, "Payment Method Updated")
-    }
-    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController,
-                                            didAuthorizePayment payment: PKPayment,
-                                            handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
-        logger.log(.debug, "Payment Was Authorized")
-        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
-    }
-    func paymentAuthorizationViewControllerWillAuthorizePayment(_ controller: PKPaymentAuthorizationViewController) {
-        logger.log(.debug, "Payment is being authorized.")
-    }
-}
-
-extension CreditsVC {
-    func itemToSell(amount: Double, label: Int) -> [PKPaymentSummaryItem] {
-        let totalPrice = PKPaymentSummaryItem(label: "\(label) \(L10n.Credit.title)",
-            amount: NSDecimalNumber(string: "\(amount)"))
-        return [totalPrice]
-    }
-    
-    func makePayment(price: Double, numberOfCredit: Int) {
-        logger.log(.debug, "credit price: \(price) & number of credits: \(numberOfCredit)")
-        let paymentNetworks = [PKPaymentNetwork.visa, .masterCard]
-        if PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: paymentNetworks) {
-            let paymentRequest = PKPaymentRequest()
-            paymentRequest.currencyCode = Constants.currencyCode
-            paymentRequest.countryCode = Constants.countryCode
-            paymentRequest.merchantIdentifier = Constants.merchantId
-            paymentRequest.merchantCapabilities = .capability3DS
-            paymentRequest.supportedNetworks = paymentNetworks
-            paymentRequest.requiredShippingContactFields = []
-            paymentRequest.paymentSummaryItems = self.itemToSell(amount: price, label: numberOfCredit)
-            paymentRequest.shippingMethods = .none
-            
-            let applePayVC = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest)
-            applePayVC?.delegate = self
-            self.present(applePayVC!, animated: true, completion: nil)
-        } else {
-            LocalNotificationView.shared.showWarning(L10n.Common.warning, body: L10n.Applepay.setAccount)
-        }
     }
 }
