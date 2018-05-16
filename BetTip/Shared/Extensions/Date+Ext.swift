2669b5f6fb373d48dd2710e01b0ccd207985937c
Haydar Karkin <haydar.karkin@gmail.com>
2018-05-17 00:23:51 +0300

    Updated extensions and debug detector

--- BetTip/Shared/Extensions/Date+Ext.swift
+++ BetTip/Shared/Extensions/Date+Ext.swift
@@ -57,3 +57,13 @@ extension NSDate {
         return NSDate().addingTimeInterval(interval)
     }
 }
+
+extension TimeInterval {
+    static var second: TimeInterval { return 1 }
+    static var minute: TimeInterval { return second * 60 }
+    static var hour: TimeInterval { return minute * 60 }
+    static var day: TimeInterval { return hour * 24 }
+    static var week: TimeInterval { return day * 7 }
+    static var month: TimeInterval { return day * 30 }
+    static var year: TimeInterval { return day * 365 }
+}
