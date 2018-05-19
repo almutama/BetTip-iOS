606e9dac3545f05a57ddbef304fc50462e2ac8c6
Haydar Karkin <haydar.karkin@gmail.com>
2018-05-19 16:16:31 +0300

    Made separated notification funcs

--- BetTip/AppDelegate.swift
+++ BetTip/AppDelegate.swift
@@ -29,43 +29,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.registerServices()
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-    }
     
-    func setupNotification(application: UIApplication) {
-        if #available(iOS 10.0, *) {
-            // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-            
-            // For iOS 10 data message (sent via FCM)
-            Messaging.messaging().delegate = self
-            
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+        #if DEBUG || ADHOC || INHOUSE
+        if !HockeyAppManager().isIdentificationPassed {
+            let identify = HockeyAppManager().identify()
+            identify { identified, error in
+                if !identified, let error = error {
+                    self.logger.log(.debug, error.localizedDescription)
+                }
+            }
         }
-        
-        application.registerForRemoteNotifications()
-        
+        #endif
     }
     
     func registerServices() {
@@ -84,57 +60,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         IQKeyboardManager.shared.enable = true
         
         // MARK: Reports
-        //HockeyApp().setupAuth()
-    }
-}
-
-extension AppDelegate {
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        var token: String = ""
-        for i in 0..<deviceToken.count {
-            token += String(format: "%02.2hhx", deviceToken[i] as CVarArg)
-        }
-        print("APN token is: ", token)
-        Messaging.messaging().setAPNSToken(deviceToken, type: .unknown)
-    }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-        print(userInfo)
-    }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-    }
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    
-    // Receive displayed notifications for iOS 10 devices.
-    @available(iOS 10.0, *)
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        let userInfo = notification.request.content.userInfo
-        // Print message ID.
-        print("Message ID: \(String(describing: userInfo["gcm.message_id"]))")
+        #if DEBUG || ADHOC || INHOUSE
+        HockeyAppManager().setupAuth()
+        #endif
+        AnalyticsManager.sharedManager.register(newService: FirebaseAnalytics())
+        AnalyticsManager.sharedManager.register(newService: AnalyticsLogger())
         
-        // Print full message.
-        print("%@", userInfo)
-    }
-}
-
-extension AppDelegate: MessagingDelegate {
-    
-    public func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
-        connectToMessaging()
-    }
-    
-    // Receive data message on iOS 10 devices.
-    func application(received remoteMessage: MessagingRemoteMessage) {
-        print("%@", remoteMessage.appData)
-    }
-    
-    func connectToMessaging() {
-        Messaging.messaging().shouldEstablishDirectChannel = true
+        // MARK: Payment Transaction
+        PurchaseService().completeTransactions()
     }
 }
